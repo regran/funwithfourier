38243b7ceb452a75aa323918105d645dc3bb04cb
regran <rgranovskiy@gmail.com>
2016-06-17 23:30:25 -0400

    Update audin.java
    Updated and hopefully fully functional audio input

--- audin.java
+++ audin.java
@@ -1,24 +1,40 @@
 import java.io.*;
 import javax.sound.sampled.*;
 
-
+/* 		some stuff for reference	
+				int sampleSize=form.getSampleSizeInBits(); //bits in a sample
+				float sampleRate=form.getSampleRate(); //samples per second, Hz
+				int timeInterval = audioLength/samples; //time between samples */
 
 public class audin{
-	File testFile = new File("intro.wav");
-	int frames=0;
-	try{
-		AudioInputStream inp=AudioSystem.getAudioInputStream(testFile);
-		
-		AudioFileFormat form = AudioSystem.getAudioFileFormat(testFile);
+	
+	private File f;
+	private AudioFormat form;
+	private AudioInputStream inp;
+	
+	public audin(String s){ //construct input with format info using file name
+		f=new File(s);
 		try{
-			int frameSize=form.getFrameSize();
-			if(framesize==AudioSystem.NOT_SPECIFIED){
-				throw(new unspecifiedframe());
-			}
-		}catch(unspecifiedframe e){
-			System.out.println("This format does not have a specified frame size.")
+			inp=AudioSystem.getAudioInputStream(f);
+			form=inp.getFormat();
+		} catch(Exception e){
+			System.out.println("Something went wrong");
 		}
-		int numBytes=1024*frameSize;
+		
+	}
+	
+	public AudioFormat getForm(){
+		return form;
+	}
+	
+	public AudioInputStream getStream(){
+		return inp;
+	}
+	
+	public byte[] getdata(){
+		int frames=0;
+		int frameSize=form.getFrameSize(); //number of samples in a frame
+		int numBytes=1024*frameSize; //1024 samples in array
 		byte[] audioBytes = new byte[numBytes];
 		try{
 			int framesread=0;
@@ -27,12 +43,44 @@ public class audin{
 				framesread=bytesread/frameSize;
 				frames+=framesread;
 			}
+		return audioBytes;
 		} catch(Exception e){
 			System.out.println("RIP");
 		}
-	} catch(UnsupportedAudioFileException e){
-		System.out.println("This file is unsupported. You should use waves tbh");
-	} catch(Exception e){
-		System.out.println("ERROR.");
+
+		return new byte[1];
+	}
+	
+	public double[] toDouble(byte[] b){ //precond: byte array of audio data
+		//semi-redundant references
+		float frameLength=inp.getFrameLength(); //frames in sample
+		float frameRate=form.getFrameRate(); //frames per second
+		float audioLength=frameLength/frameRate; //length of sample in seconds
+		float sampleRate=form.getSampleRate(); //samples per second, Hz
+		int samples = (int)(audioLength*sampleRate/2);  //n
+
+		boolean isBig = form.isBigEndian(); //little or big endian format
+		double[] dubs = new double[1024]; //arbitrary-ish length for now
+		for(int i=0; i<dubs.length*2; i+=2){ 	//convert pairs of bytes to Endian values
+			int b1=b[i];
+			int b2=b[i+1];
+			if(b1<0) b1+= 0x100;
+			if(b2<0) b2+=0x100;
+			
+			if(isBig) dubs[i/2] = b1 + (b2<<8);
+			else dubs[i/2]=(b1<<8) + b2;
+		}
+		return dubs; //postcond: samples as doubles
+	}
+	public static void main(String[] args){
+		audin input = new audin("intro.wav");
+		byte[] b = input.getdata();
+		double[] d = input.toDouble(b);
+		Complex[] comp = Complex.makeComp(d);
+		Complex[] p = fft.pad(comp);
+		Complex[] freq = fft.transform(p);
+		for(int k=0; k<p.length; k++) {
+			Complex.printlnComp(freq[k]);
+		}
 	}
 }
